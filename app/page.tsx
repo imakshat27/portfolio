a1b322531d6d60a746d33d81678a53fac568e9c0
Akshat <agarwalakshat2710@gmail.com>
2026-01-22 16:48:32 +0530

    feat: add LightRays component for dynamic visual effects and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import PillNav from '@/components/PillNav';
+import LightRays from '@/components/LightRays';
 
 export default function Home() {
   return (
     <>
+    
     <div className="flex justify-center bg-zinc-50 font-sans dark:bg-black min-h-screen">
       <PillNav
         logo='/logo.svg'
