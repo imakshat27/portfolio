7f2fb79d1c47a1620f6b9326393f8898941f8f95
Akshat <agarwalakshat2710@gmail.com>
2026-01-24 16:39:32 +0530

    feat: update social media icons with links and replace profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -47,12 +47,12 @@ export default function Home() {
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-blue-100">Akshat<br/>Agarwal</h1>
         <p className="text-lg sm:text-xl md:text-2xl font-bold text-blue-200 mt-4">Web Developer</p>
         <div className="flex justify-center lg:justify-center gap-6 mt-6">
-        <img src="/github.png" alt="Github" className="w-10 h-10 sm:w-10 sm:h-10" />
-        <img src="/linkedin.png" alt="LinkedIn" className="w-10 h-10 sm:w-10 sm:h-10" />
-        <img src="/twitter.png" alt="Twitter" className="w-10 h-10 sm:w-10 sm:h-10" />
+        <a href="https://github.com/imakshat27"><img src="/github.png" alt="Github" className="w-10 h-10 sm:w-10 sm:h-10" /></a>
+        <a href="https://linkedin.com/in/imakshat27"><img src="/linkedin.png" alt="LinkedIn" className="w-10 h-10 sm:w-10 sm:h-10" /></a>
+        <a href="https://x.com/imakshat_27"><img src="/twitter.png" alt="Twitter" className="w-10 h-10 sm:w-10 sm:h-10" /></a>
         </div>
       </div>
-      <img src="/image.png" alt="Profile Picture" className="w-48 sm:w-64 md:w-80 lg:w-96 h-auto rounded-full" />
+      <img src="/carricature.png" alt="Profile Picture" className="w-48 sm:w-64 md:w-70 lg:w-70 h-auto rounded-full" />
       </div>
       
       
