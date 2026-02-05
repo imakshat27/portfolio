d479327477b7efa463f5e7d00b1c704c0061f919
Akshat <agarwalakshat2710@gmail.com>
2026-02-05 20:00:40 +0530

    feat: remove vestigial items array from infinitescroll component

--- components/ui/Projects.tsx
+++ components/ui/Projects.tsx
@@ -1,32 +1,6 @@
 import TiltedCard from "../TiltedCard";
 
 export default function Projects(){
-    const items = [
-  {
-    image: 'https://picsum.photos/300/300?grayscale',
-    link: 'https://github.com/imakshat27',
-    title: 'Project 1',
-    description: 'This is pretty cool, right?'
-  },
-  {
-    image: 'https://picsum.photos/400/400?grayscale',
-    link: 'https://github.com/imakshat27',
-    title: 'Project 2',
-    description: 'This is pretty cool, right?'
-  },
-  {
-    image: 'https://picsum.photos/500/500?grayscale',
-    link: 'https://github.com/imakshat27',
-    title: 'Project 3',
-    description: 'This is pretty cool, right?'
-  },
-  {
-    image: 'https://picsum.photos/600/600?grayscale',
-    link: 'https://github.com/imakshat27',
-    title: 'Project 4',
-    description: 'This is pretty cool, right?'
-  }
-];
     return (
         <>
         <div className="text-2xl items-center font-semibold text-center md:text-4xl lg:text-5xl text-black dark:text-white">
